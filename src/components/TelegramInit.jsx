22ff4d9ff4c41c8973d6ccd97ce0f9f873f6cece
MoeinRobati <moeinrobati13861386@gmail.com>
2025-09-04 02:51:17 +0430

    Update Gocher bots

--- src/components/TelegramInit.jsx
+++ src/components/TelegramInit.jsx
@@ -1,22 +1,12 @@
 'use client';
+
 import { useEffect } from 'react';
 import { init } from '@telegram-apps/sdk';
 
-export default function TelegramInit() {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      try {
-        const tg = init();
-        console.log("Telegram initialized:", tg);
-        console.log("InitData:", tg.initData);
-        console.log("User:", tg.user);
-        console.log("Platform:", tg.platform);
-        console.log("ColorScheme:", tg.colorScheme);
-      } catch (error) {
-        console.log("Not running inside Telegram:", error.message);
-      }
-    }
-  }, []);
+export function TelegramInit() {
+    useEffect(() => {
+        init();
+    }, []);
 
-  return null;
+    return null; 
 }
