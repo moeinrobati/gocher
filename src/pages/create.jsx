bace53a312d7dd8ed49c09cbb98e7f8db78a314e
MoeinRobati <moeinrobati13861386@gmail.com>
2025-09-14 16:19:34 +0430

    Update Gocher bots

--- src/pages/create.jsx
+++ src/pages/create.jsx
@@ -1,18 +1,12 @@
 'use client'
 
-import TelegramInit from "../components/TelegramInit";
+import TelegramInit from '../components/TelegramInit';
 import CopyInitData from '../components/CopyInitData';
 
 export default function CreatePage() {
-  return (
-    <div style={{ padding: 100 }}>
-      <h1>Create Page</h1>
-
-      {/* این کامپوننت init() تلگرام را فراخوانی می‌کند */}
-      <TelegramInit />
-
-      {/* این کامپوننت initData را نمایش و کپی می‌کند */}
-      <CopyInitData />
+  return 
+    <div>
+      <TelegramInit/>
     </div>
-  )
+  
 }
