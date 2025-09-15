0b121c02aae957dda96198ef7faeec03cc7b8b7e
MoeinRobati <moeinrobati13861386@gmail.com>
2025-09-15 16:46:30 +0430

    Update Gocher bot

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,11 +1,15 @@
-import TelegramInit from '../components/TelegramInit';
+'use client';
 
-import CopyInitData from '../components/CopyInitData';
+import dynamic from 'next/dynamic';
+
+const TelegramInit = dynamic(() => import('../components/TelegramInit'), { ssr: false });
+const CopyInitData = dynamic(() => import('../components/CopyInitData'), { ssr: false });
+
 export default function Layout({ children }) {
   return (
     <>
-      <TelegramInit/>
-      <CopyInitData/>
+      <TelegramInit />
+      {process.env.NODE_ENV === 'development' && <CopyInitData />}
       {children}
     </>
   );
